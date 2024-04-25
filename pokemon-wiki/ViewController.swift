f404e0535cce0abd28ba24029f34d2e2d97eb28b
mildsupanat <supanat.wanroj@gmail.com>
2024-04-25 16:19:12 +0700

    init project

---
+++ pokemon-wiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemon-wiki
+//
+//  Created by Supanat Wanroj on 25/4/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
