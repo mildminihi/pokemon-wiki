68898c7385987e3029ad75755c01825896df7bb9
mild supanat <mildsupanat@gmail.com>
2024-04-30 16:27:57 +0700

    Delete Pods directory

--- Pods/Target Support Files/Pods-pokemon-wiki-pokemon-wikiUITests/Pods-pokemon-wiki-pokemon-wikiUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pokemon_wiki_pokemon_wikiUITests : NSObject
-@end
-@implementation PodsDummy_Pods_pokemon_wiki_pokemon_wikiUITests
-@end
--- Pods/Target Support Files/Pods-pokemon-wiki/Pods-pokemon-wiki-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pokemon_wiki : NSObject
-@end
-@implementation PodsDummy_Pods_pokemon_wiki
-@end
